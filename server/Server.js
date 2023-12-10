d14c16d5103632458ba7ccbe5fe1b6b1f6a96874
Tejas Vivek Palyekar <tejaspalyekar18@gmail.com>
2023-12-10 13:42:03 +0530

    commit

--- server/Server.js
+++ server/Server.js
@@ -1,18 +1,23 @@
+// Importing required modules
 const app = require('express')();
 const httpServer = require('http').Server(app);
 const io = require('socket.io')(httpServer);
 
+// Set up socket.io to listen for connections
 io.on('connection', (socket) => {
-  console.log('user connected');      //when user is connected
+  console.log('user connected');      
+  // Listen for 'chat_message' event from a client
   socket.on('chat_message', (msg) => {
     console.log('message: ' + msg);
-    io.emit('chat_message', msg);
+    io.emit('chat_message', msg); // Broadcast the message to all connected clients
   });
+  // Listen for 'disconnect' event when a user disconnects
   socket.on('disconnect', () => {
-    console.log('user disconnected'); //user disconnected
+    console.log('user disconnected'); 
   });
 });
 
+// Start the HTTP server and listen on port 3000
 httpServer.listen(3000, () => {
   console.log('listening on *:3000');
 });
